04b4990f542bbcd3889a6e31c80e2d12a32dc27d
gbelot2003 <gbelot2003@hotmail.com>
2018-08-12 21:22:12 -0600

    Agregando logica de user_id en creacion de Productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -16,8 +17,20 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
+        $request['user_id'] = Auth::id();
         $product = Product::create($request->all());
 
         return $product;
+    }
+
+    public function destroy($id)
+    {
+        try{
+            $destroyed = Product::findOrFail($id);
+            Product::destroy($id);
+            return response($destroyed, 204);
+        } catch (\Exception $s){
+            return response(['Problem deleting the product'], 500);
+        }
     }
 }
